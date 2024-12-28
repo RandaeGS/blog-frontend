b82087b36ac80297ebd86d0f69916c206b3c6140
RandaeGS <garciasanchezrd@gmail.com>
2024-12-28 15:40:07 -0400

    finish article item style

--- src/components/listBlogs.vue
+++ src/components/listBlogs.vue
@@ -7,13 +7,17 @@
 
     <div>
       <div class="border-b border-slate-200 pb-2 mb-2">
-        <h1 class="text-2xl font-semibold text-slate-800">Titulo</h1>
+        <h1 class="text-2xl font-semibold text-slate-800">Example title</h1>
       </div>
 
       <div>
-        <p>Contenido</p>
-        <button class="bg-blue-500 hover:bg-blue-700 rounded px-2">Update</button>
-        <button class="bg-red-500 hover:bg-red-700 rounded px-2 ml-2">Delete</button>
+        <p class="leading-relaxed">Example content</p>
+
+        <div class="flex gap-2 mt-4">
+          <button
+            class="px-4 py-2 bg-blue-500 hover:bg-blue-700 rounded-md text-white transition-colors">Update</button>
+          <button class="px-4 py-2 bg-red-500 hover:bg-red-700 rounded-md text-white transition-colors">Delete</button>
+        </div>
       </div>
     </div>
 
