d31b66a41ed45a27651d9ae1db17ad9373c17aff
RandaeGS <garciasanchezrd@gmail.com>
2024-12-28 15:32:08 -0400

    style article title

--- src/components/listBlogs.vue
+++ src/components/listBlogs.vue
@@ -3,8 +3,19 @@
 </script>
 
 <template>
-  <div class="p-4 mx-auto bg-slate-400">
-    <h1>hola</h1>
+  <div class="p-6 bg-slate-100 shadow-md mx-auto ">
 
-  </div class="mx-auto bg-slate-400">
+    <div>
+      <div class="border-b border-slate-200 pb-2 mb-2">
+        <h1 class="text-2xl font-semibold text-slate-800">Titulo</h1>
+      </div>
+
+      <div>
+        <p>Contenido</p>
+        <button class="bg-blue-500 hover:bg-blue-700 rounded px-2">Update</button>
+        <button class="bg-red-500 hover:bg-red-700 rounded px-2 ml-2">Delete</button>
+      </div>
+    </div>
+
+  </div>
 </template>
