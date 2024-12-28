97a255d64af12b77ccf9ed9389a86fcc66206c68
RandaeGS <garciasanchezrd@gmail.com>
2024-12-28 15:58:06 -0400

    add style to navbar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <script setup>
+import listBlogs from './components/listBlogs.vue'
 
 </script>
 
 <template>
-  <div class="">
-    <nav class="bg-indigo-300">
+  <div class="min-h-screen bg-gray-50 flex flex-col">
+
+    <nav class="bg-indigo-600 shadow-md">
       <div class="font-semibold flex">
-        <a class="p-4 hover:bg-indigo-600">List</a>
-        <a class="p-4 hover:bg-indigo-600">Create</a>
+        <a class="p-4 hover:bg-indigo-700 transition-colors text-white">List</a>
+        <a class="p-4 hover:bg-indigo-700 transition-colors text-white">Create</a>
       </div>
     </nav>
+
+    <main>
+      <listBlogs />
+    </main>
   </div>
 </template>
 
