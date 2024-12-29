72bf0eed39c7c4df12bde7edbf161b8ab77ebc2b
RandaeGS <garciasanchezrd@gmail.com>
2024-12-29 13:13:27 -0400

    finish list articles functionality

--- src/components/listBlogs.vue
+++ src/components/listBlogs.vue
@@ -16,13 +16,18 @@ onMounted(() => {
 
 <template>
 
-  <div>
+  <div v-if="listArticles.length === 0">
+    <h1 class="text-center font-bold text-2xl py-2">No articles have been written</h1>
+  </div>
+
+  <div v-for="article in listArticles">
     <div class="border-b border-slate-200 pb-2 mb-2">
-      <h1 class="text-2xl font-semibold text-slate-800">Example title</h1>
+      <h1 class="text-2xl font-semibold text-slate-800">{{ article.title }}</h1>
+      <h2 class="text-gray-600 pt-1">{{ article.authorName }}</h2>
     </div>
 
-    <div>
-      <p class="leading-relaxed">Example content</p>
+    <div class="mb-6">
+      <p class="leading-relaxed">{{ article.content }}</p>
 
       <div class="flex gap-2 mt-4">
         <button class="px-4 py-2 bg-blue-500 hover:bg-blue-600 rounded-md text-white transition-colors">Update</button>
