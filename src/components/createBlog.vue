e9f61d0c315dbc9c82f8348b9990b18601e98461
RandaeGS <garciasanchezrd@gmail.com>
2024-12-30 19:04:54 -0400

    finish article creation

--- src/components/createBlog.vue
+++ src/components/createBlog.vue
@@ -1,4 +1,45 @@
 <script setup>
+import Swal from 'sweetalert2'
+import { ref } from 'vue'
+
+const title = ref('')
+const author = ref('')
+const content = ref('')
+
+async function submit() {
+  try {
+    const res = await fetch(`http://localhost:8080/articles`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title: title.value,
+        authorName: author.value,
+        content: content.value
+      })
+    })
+
+    if (!res.ok) throw new Error('Error saving article')
+
+    await Swal.fire({
+      title: "Success",
+      text: "Article created successfully!",
+      icon: "success"
+    })
+
+    title.value = ''
+    author.value = ''
+    content.value = ''
+
+  } catch (error) {
+    await Swal.fire({
+      title: "Error",
+      text: "Article could not be created!",
+      icon: "error"
+    })
+  }
+}
 
 </script>
 
@@ -6,17 +47,24 @@
   <div class="bg-white shadow-md rounded-md w-full p-6">
     <h1 class="text-center text-2xl font-semibold py-2 mb-6">Create Article</h1>
 
-    <form action="" method="post" class="max-w-lg mx-auto">
+    <form @submit.prevent="submit" class="max-w-lg mx-auto">
       <div class="mb-4">
         <label for="title" class="block py-2">Title</label>
-        <input id="title" type="text"
+        <input v-model="title" id="title" type="text"
           class="w-full shadow appearance-none border border-indigo-200 rounded py-2 px-3 text-black"
           placeholder="Title">
       </div>
 
       <div class="mb-4">
+        <label for="author" class="block py-2">Author</label>
+        <input v-model="author" id="author" type="text"
+          class="w-full shadow appearance-none border border-indigo-200 rounded py-2 px-3 text-black"
+          placeholder="Author name">
+      </div>
+
+      <div class="mb-4">
         <label for="content" class="block py-2">Content</label>
-        <textarea id="content" type="textarea" rows="20"
+        <textarea v-model="content" id="content" type="textarea" rows="20"
           class="w-full shadow appearance-none border border-indigo-200 rounded py-2 px-3 text-black"
           placeholder="Enter article content"></textarea>
       </div>
